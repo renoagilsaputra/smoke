07aead87fa1c9da477f9fc5a44212f58be7281ca
Reno Agil Saputra <renoagilsaputra87@gmail.com>
2020-01-16 18:52:18 +0700

    login

--- app/src/main/java/com/ppb/tubes/smoke/MainActivity.kt
+++ app/src/main/java/com/ppb/tubes/smoke/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.ppb.tubes.smoke
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ppb.tubes.smoke.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_login.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
